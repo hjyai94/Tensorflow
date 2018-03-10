72ac15f505ebfea420a7a37f618af8086df52574
hjyai94 <hjyai94@163.com>
2018-03-10 10:25:27 +0800

    add new scripts

---
+++ retrain/retrain.sh
@@ -0,0 +1,8 @@
+python /home/hjy/Myproject/tensorflow/tensorflow/tensorflow/examples/image_retraining/retrain.py \
+--bottleneck_dir=bottleneck \
+--how_many_training_steps=200 \
+--model_dir=/home/hjy/Myproject/tensorflow/inception_model/ \
+--output_graph=output_graph.pb \
+--output_labels=output_labels.txt \
+--image_dir=dataset/
+
